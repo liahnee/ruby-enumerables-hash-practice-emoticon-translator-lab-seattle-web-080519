4a2ad75fda546bb6d984bd2fa79b8a4649e585ea
Catherine Yang <liahnee@gmail.com>
2019-07-23 20:33:19 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,7 +4,7 @@ def load_library
   # code goes here
   emoticon = YAML.load_file('emoticons.yml')
 #  emoticon_lib = {:get_meaning => {japanese emoticon => meaning}, :get_emoticon => {english emoticon => japanese emoticon}}
-  
+# emoticon.yml = {:angel => [""  
   
   return 
 end
