d405cf2c7191dbd0dfcd08da1f6eda1b894ba1c5
Catherine Yang <liahnee@gmail.com>
2019-07-23 21:52:03 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,7 +6,7 @@ def load_library(address)
   #  emoticon_lib = {:get_meaning => {japanese emoticon => meaning}, :get_emoticon => {english emoticon => japanese emoticon}}
   # emoticon.yml = {:angel => ["O:)", "☜(⌒▽⌒)☞"   ], :angry => [">:(", "ヽ(ｏ`皿′ｏ)ﾉ"], :bored => [].... 
   
-  emoticon_yml = YAML.load_file(address)
+  emoticon_yml = YAML.load(address)
   emoticon_yml.each do |(meaning, value_array)|
     emoticon_lib[:get_emoticon][value_array[0]] = value_array[1]
     emoticon_lib[:get_meaning][value_array[1]] = meaning
