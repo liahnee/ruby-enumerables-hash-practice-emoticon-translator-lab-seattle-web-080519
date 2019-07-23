c6c1d5a7a1542b489e0e8f5fdaa04d79cf6ed894
Catherine Yang <liahnee@gmail.com>
2019-07-23 22:04:17 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,11 +6,11 @@ def load_library(file_path)
   #  emoticon_lib = {:get_meaning => {japanese emoticon => meaning}, :get_emoticon => {english emoticon => japanese emoticon}}
   # emoticon.yml = {:angel => ["O:)", "☜(⌒▽⌒)☞"   ], :angry => [">:(", "ヽ(ｏ`皿′ｏ)ﾉ"], :bored => [].... 
 
-  emoticon_lib = {:get_meaning => {}, :get_emoticon => {}}
+  emoticon_lib = {"get_meaning" => {}, "get_emoticon" => {}}
   emoticon_yml = YAML.load_file(file_path)
   emoticon_yml.each do |(meaning, value_array)|
-    emoticon_lib[:get_emoticon][value_array[0]] = value_array[1]
-    emoticon_lib[:get_meaning][value_array[1]] = meaning
+    emoticon_lib["get_emoticon"][value_array[0]] = value_array[1]
+    emoticon_lib["get_meaning"][value_array[1]] = meaning
   end
   return emoticon_lib
 end
@@ -19,7 +19,7 @@ end
 def get_japanese_emoticon(address, arg_eng_emo)
   # code goes here
   emo_hash = load_library(address)
-  emo_hash[:get_emoticon].each do |(eng_emo, jap_emo)|
+  emo_hash["get_emoticon"].each do |(eng_emo, jap_emo)|
     if eng_emo == arg_eng_emo
       return jap_emo
     end
@@ -31,7 +31,7 @@ end
 def get_english_meaning(address, arg_jap_emo)
   # code goes here
   emo_hash = load_library(address)
-  emo_hash[:get_meaning].each do |(jap_emo, meaning)|
+  emo_hash["get_meaning"].each do |(jap_emo, meaning)|
     if jap_emo == arg_jap_emo
       return meaning
     end
