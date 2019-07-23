1b3f215ab1863a7869e5eb82279e75a2a4a39b37
Catherine Yang <liahnee@gmail.com>
2019-07-23 20:57:48 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,22 +1,23 @@
 # require modules here
 
-def load_library
+def load_library(address)
   # code goes here
   #  emoticon_lib = {:get_meaning => {japanese emoticon => meaning}, :get_emoticon => {english emoticon => japanese emoticon}}
   # emoticon.yml = {:angel => ["O:)", "☜(⌒▽⌒)☞"   ], :angry => [">:(", "ヽ(ｏ`皿′ｏ)ﾉ"], :bored => [].... 
   
   
-  emoticon = YAML.load_file('emoticons.yml')
-  emoticon.each do |(meaning, value_array)|
+  emoticon_yml = YAML.load_file(address)
+  emoticon_yml.each do |(meaning, value_array)|
     emoticon_lib[:get_emoticon][value_array[0]] = value_array[1]
     emoticon_lib[:get_meaning][value_array[1]] = meaning
   end
-  return emoticon
+  return emoticon_lib
 end
 
-def get_japanese_emoticon
+def get_japanese_emoticon(address, emoticon_eng)
   # code goes here
-  
+  emo_hash = load_library(address)
+    if 
   
 end
 
